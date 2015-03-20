ba1896a6dc1e2cd997d7cdf7799c686a03d18fb3
AdriaRios <adrianrios89@gmail.com>
2015-03-20 09:17:59 +0100

    Added Resumen

--- week1/deepComparision.js
+++ week1/deepComparision.js
@@ -7,8 +7,6 @@ var deepEqual = function (obj1, obj2){
 	return JSON.stringify(obj1) == JSON.stringify(obj2);
 }
 
-
-
 var obj = {here: {is: "an"}, object: 2};
 console.log(JSON.stringify(obj) );
 console.log(deepEqual(obj, obj));
