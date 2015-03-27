4afe3b0272841880a75ef2e63c56373bdab25953
AdriaRios <adrianrios89@gmail.com>
2015-03-27 10:52:58 +0100

    Week4, Flappy Bird

--- week2/EloquentJavascript6_2.js
+++ week2/EloquentJavascript6_2.js
@@ -12,6 +12,7 @@ Vector.prototype.plus = function(vector) {
   return new Vector(this.x+vector.x, this.y+vector.y);
 };
 
+
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 
